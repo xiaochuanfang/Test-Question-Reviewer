9da56139328abc10baeb71855506e798fff16128
xiaochuanfang <xiaochuan1991@hotmail.com>
2020-03-23 19:48:50 -0400

    Feature added: support upto 5 multiple choices

--- Test-Question-Reviewer/src/Test.java
+++ Test-Question-Reviewer/src/Test.java
@@ -19,6 +19,8 @@ public class Test extends JFrame {
 	private JButton btnChoice2;
 	private JButton btnChoice3;
 	private JButton btnChoice4;
+	private JButton btnChoice5;
+	private ArrayList<JButton> blist;
 	private ArrayList<Question> qlist;
 	private ArrayList<Score> slist;
 	private int qNumber;
@@ -30,6 +32,7 @@ public class Test extends JFrame {
 		
 		//Initialized question number,score list and question list
 		qNumber=0;
+		blist=new ArrayList<JButton>();
 		slist=new ArrayList<Score>();
 		this.qlist=qlist;
 		
@@ -55,6 +58,8 @@ public class Test extends JFrame {
 		btnChoice1.setHorizontalAlignment(SwingConstants.LEFT);
 		btnChoice1.setFont(new Font("Monospaced", Font.PLAIN, 25));
 		btnChoice1.setBounds(15, 401, 795, 81);
+		btnChoice1.setVisible(false);
+		blist.add(btnChoice1);
 		contentPane.add(btnChoice1);
 		
 		btnChoice2 = new JButton("");
@@ -62,6 +67,8 @@ public class Test extends JFrame {
 		btnChoice2.setHorizontalAlignment(SwingConstants.LEFT);
 		btnChoice2.setFont(new Font("Monospaced", Font.PLAIN, 25));
 		btnChoice2.setBounds(15, 498, 795, 81);
+		btnChoice2.setVisible(false);
+		blist.add(btnChoice2);
 		contentPane.add(btnChoice2);
 		
 		btnChoice3 = new JButton("");
@@ -69,6 +76,8 @@ public class Test extends JFrame {
 		btnChoice3.setHorizontalAlignment(SwingConstants.LEFT);
 		btnChoice3.setFont(new Font("Monospaced", Font.PLAIN, 25));
 		btnChoice3.setBounds(15, 595, 795, 81);
+		btnChoice3.setVisible(false);
+		blist.add(btnChoice3);
 		contentPane.add(btnChoice3);
 		
 		btnChoice4 = new JButton("");
@@ -76,11 +85,22 @@ public class Test extends JFrame {
 		btnChoice4.setHorizontalAlignment(SwingConstants.LEFT);
 		btnChoice4.setFont(new Font("Monospaced", Font.PLAIN, 25));
 		btnChoice4.setBounds(15, 692, 795, 81);
+		btnChoice4.setVisible(false);
+		blist.add(btnChoice4);
 		contentPane.add(btnChoice4);
+		
+		btnChoice5 = new JButton("");
+		btnChoice5.setName("E");
+		btnChoice5.setHorizontalAlignment(SwingConstants.LEFT);
+		btnChoice5.setFont(new Font("Monospaced", Font.PLAIN, 25));
+		btnChoice5.setBounds(15, 787, 795, 81);
+		btnChoice5.setVisible(false);
+		blist.add(btnChoice5);
+		contentPane.add(btnChoice5);
 		
 		//Set frame operation and position
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1354, 862);
+		setBounds(100, 100, 1530, 932);
 		setContentPane(contentPane);
 		setVisible(true);
 		
@@ -115,6 +135,7 @@ public class Test extends JFrame {
 		btnChoice2.addActionListener(listener);
 		btnChoice3.addActionListener(listener);
 		btnChoice4.addActionListener(listener);
+		btnChoice5.addActionListener(listener);
 	
 		//Load text on the text area and buttons
 		loadQuestion();
@@ -156,11 +177,16 @@ public class Test extends JFrame {
 		//Get current question number
 		Question question=qlist.get(qNumber);
 			
-		//Load the text for the statement and multiple choices
+		//Load the text for the statement
 		taStatement.setText(question.getStatement());
-		btnChoice1.setText(question.getChoices()[0]);
-		btnChoice2.setText(question.getChoices()[1]);
-		btnChoice3.setText(question.getChoices()[2]);
-		btnChoice4.setText(question.getChoices()[3]);
+		
+		//Load the button and text for the multiple choices
+		int NumOfChoice=question.getChoices().length;
+		int count=0;
+		while(count!=NumOfChoice) {
+			blist.get(count).setVisible(true);
+			blist.get(count).setText(question.getChoices()[count]);
+			count++;
+		}
 	}
 }
